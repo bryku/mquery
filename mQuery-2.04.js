68605546dcfb9eb84b1a8ffa6e47fafd196b721d
bryku <drburnett@mail.com>
2020-11-09 10:01:24 -0800

    Update mQuery-2.04.js

--- mQuery-2.04.js
+++ mQuery-2.04.js
@@ -112,7 +112,7 @@ var _proto = {
 			else if(!a[1] && a[0] == 'style'){return e.style.cssText}
 			else if(!a[1]){return e.getAttribute(a[0])}
 			else if(a[0] == 'value'){e.value = a[1]}
-			else if(a[0] == 'style'){e.value = a[1]}
+			else if(a[0] == 'style'){e.style.cssText = a[1]}
 			else{e.setAttribute(a[0],a[1])}
 		},arguments)
 	},
